ec9bb589ecac18e481709616fbcf46ab184f6733
katamartin <katasm@gmail.com>
2023-01-04 21:31:00 -0800

    Wire up form to rechunking service

--- components/dataset.js
+++ components/dataset.js
@@ -1,10 +1,10 @@
-import { Select } from '@carbonplan/components'
+import { Input, Select } from '@carbonplan/components'
 import { Flex } from 'theme-ui'
+import { useCallback, useEffect, useState } from 'react'
 import Label from './label'
 import { TooltipContent, TooltipWrapper } from './tooltip'
 import useStore from './store'
 import VariableMetadata from './variable-metadata'
-import { useState } from 'react'
 
 const DATASETS = [
   'https://storage.googleapis.com/carbonplan-maps/ncview/demo/single_timestep/air_temperature.zarr',
@@ -35,36 +35,95 @@ const sx = {
     },
   },
 }
+
+const createDataset = async (url) => {
+  const res = await fetch('https://ncview-backend.fly.dev/datasets/', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url, force: false }),
+  })
+  return res.json()
+}
+
+const pollForCompletedRun = async (
+  id,
+  setCompletedRun,
+  interval = 1000,
+  polls = 30
+) => {
+  const res = await fetch(`https://ncview-backend.fly.dev/datasets/${id}`, {
+    method: 'GET',
+    mode: 'cors',
+  })
+  const payload = await res.json()
+
+  if (payload.rechunk_runs?.length > 0) {
+    const run = payload.rechunk_runs[0]
+    if (run.status === 'completed') {
+      setCompletedRun(run)
+      return
+    }
+  }
+
+  if (polls > 1) {
+    setTimeout(
+      () => pollForCompletedRun(id, setCompletedRun, interval, polls - 1),
+      interval
+    )
+  }
+}
+
 const Dataset = () => {
   const [expanded, setExpanded] = useState(false)
-  const url = useStore((state) => state.url)
-  const setUrl = useStore((state) => state.setUrl)
-
+  const [url, setUrl] = useState('')
+  const [dataset, setDataset] = useState(null)
+  const [completedRun, setCompletedRun] = useState(null)
+  const setStoreUrl = useStore((state) => state.setUrl)
   const variable = useStore((state) => state.variable.name)
   const variables = useStore((state) => state.variables)
 
   const setVariable = useStore((state) => state.setVariable)
 
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault()
+      setDataset(null)
+      setCompletedRun(null)
+      const d = await createDataset(url)
+      if (d.id) {
+        setDataset(d)
+        // todo: set interval + number of polls based on dataset size
+        pollForCompletedRun(d.id, setCompletedRun)
+      }
+
+      // show error
+    },
+    [url]
+  )
+
+  useEffect(() => {
+    if (dataset && completedRun) {
+      if (completedRun.outcome === 'success') {
+        setStoreUrl(completedRun.rechunked_dataset, dataset.cf_axes)
+      }
+    }
+  }, [dataset, completedRun])
+
   return (
     <Flex sx={{ flexDirection: 'column', gap: 3 }}>
-      <Label value='Dataset' htmlFor='dataset' direction='vertical'>
-        <Select
-          value={url ?? undefined}
-          onChange={(e) => setUrl(e.target.value, API_METADATA[e.target.value])}
-          id='dataset'
-          sx={sx.select}
-        >
-          <option disabled selected value>
-            select an option
-          </option>
-
-          {DATASETS.map((d, i) => (
-            <option key={d} value={d}>
-              {d}
-            </option>
-          ))}
-        </Select>
-      </Label>
+      <form onSubmit={handleSubmit}>
+        <Label value='Dataset' htmlFor='dataset' direction='vertical'>
+          <Input
+            id='dataset'
+            onChange={(e) => setUrl(e.target.value)}
+            value={url}
+            sx={{ width: '100%' }}
+          />
+        </Label>
+      </form>
 
       {variable && (
         <>
