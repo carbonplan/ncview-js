937faa353938968f9c31ee0f63461ca17d6dc2cf
katamartin <katasm@gmail.com>
2023-01-12 09:33:26 -0800

    Move keyboard events into MapContainer and check focus state

--- components/map-container.js
+++ components/map-container.js
@@ -1,13 +1,75 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { Box } from 'theme-ui'
 
-const MapContainer = ({ sx, children, onDrag, onScroll }) => {
+import useStore from './store'
+import Zoom from './zoom'
+
+const MapContainer = ({ sx, children, setMapProps }) => {
   const container = useRef(null)
   const moveListener = useRef(null)
   const [cursor, setCursor] = useState('grab')
+  const hasData = useStore((state) => !!state.data)
+
+  const panMap = useCallback((offset) => {
+    setMapProps((prev) => ({
+      scale: prev.scale,
+      translate: prev.translate.map((d, i) => d + offset[i]),
+    }))
+  }, [])
+
+  const zoomMap = useCallback((delta) => {
+    setMapProps((prev) => {
+      const updatedScale =
+        prev.scale + delta < 0 ? prev.scale : prev.scale + delta
+      return {
+        scale: updatedScale,
+        translate: prev.translate.map((d) => (d / prev.scale) * updatedScale),
+      }
+    })
+  }, [])
+
+  const handler = useCallback(
+    ({ key, keyCode, metaKey, ...rest }) => {
+      if (document.activeElement !== container.current) {
+        return
+      }
+
+      if (hasData) {
+        if (key.includes('Arrow')) {
+          const offsets = {
+            ArrowUp: [0, 1],
+            ArrowRight: [-1, 0],
+            ArrowDown: [0, -1],
+            ArrowLeft: [1, 0],
+          }
+          const offset = offsets[key]
+
+          if (!offset) {
+            throw new Error(`Unexpected arrow key: ${key}`)
+          }
+
+          panMap(offset)
+        } else if (key === '=') {
+          // zoom in
+          zoomMap(1)
+        } else if (key === '-') {
+          // zoom out
+          zoomMap(-1)
+        }
+      }
+    },
+    [hasData]
+  )
+  useEffect(() => {
+    window.addEventListener('keydown', handler)
+
+    return () => {
+      window.removeEventListener('keydown', handler)
+    }
+  }, [handler])
 
   useEffect(() => {
-    const wheelListener = (event) => onScroll(event.deltaY / -48)
+    const wheelListener = (event) => zoomMap(event.deltaY / -48)
     document.addEventListener('wheel', wheelListener)
 
     return () => {
@@ -25,11 +87,11 @@ const MapContainer = ({ sx, children, onDrag, onScroll }) => {
       container.current.removeEventListener('mousemove', moveListener.current)
     }
     moveListener.current = (event) => {
-      onDrag([event.movementX / width, event.movementY / height])
+      panMap([event.movementX / width, event.movementY / height])
     }
 
     container.current.addEventListener('mousemove', moveListener.current)
-  }, [onDrag])
+  }, [panMap])
 
   const handleMouseUp = useCallback(() => {
     setCursor('grab')
@@ -41,12 +103,14 @@ const MapContainer = ({ sx, children, onDrag, onScroll }) => {
 
   return (
     <Box
-      sx={{ cursor, ...sx }}
+      sx={{ cursor, '&:focus, &:focus-visible': { outline: 'none' }, ...sx }}
       ref={container}
+      tabIndex={0}
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
     >
       {children}
+      <Zoom zoomOut={() => zoomMap(-1)} zoomIn={() => zoomMap(1)} />
     </Box>
   )
 }
--- components/map.js
+++ components/map.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { Minimap, Path, Sphere, Raster } from '@carbonplan/minimaps'
 import { useThemeUI, Box } from 'theme-ui'
@@ -7,7 +7,6 @@ import { useThemedColormap } from '@carbonplan/colormaps'
 import { PROJECTIONS } from './constants'
 import useStore from './store'
 import { getMapProps } from './utils'
-import Zoom from './zoom'
 import MapContainer from './map-container'
 import Loading from './loading'
 import MinimapListener from './minimap-listener'
@@ -31,61 +30,6 @@ const Map = () => {
   const { northPole, nullValue } = useStore((state) => state.variable)
   const mapPropsInitialized = useRef(false)
 
-  const panMap = useCallback((offset) => {
-    setMapProps((prev) => ({
-      scale: prev.scale,
-      translate: prev.translate.map((d, i) => d + offset[i]),
-    }))
-  }, [])
-
-  const zoomMap = useCallback((delta) => {
-    setMapProps((prev) => {
-      const updatedScale =
-        prev.scale + delta < 0 ? prev.scale : prev.scale + delta
-      return {
-        scale: updatedScale,
-        translate: prev.translate.map((d) => (d / prev.scale) * updatedScale),
-      }
-    })
-  }, [])
-
-  const handler = useCallback(
-    ({ key, keyCode, metaKey }) => {
-      if (!!data) {
-        if (key.includes('Arrow')) {
-          const offsets = {
-            ArrowUp: [0, 1],
-            ArrowRight: [-1, 0],
-            ArrowDown: [0, -1],
-            ArrowLeft: [1, 0],
-          }
-          const offset = offsets[key]
-
-          if (!offset) {
-            throw new Error(`Unexpected arrow key: ${key}`)
-          }
-
-          panMap(offset)
-        } else if (key === '=') {
-          // zoom in
-          zoomMap(1)
-        } else if (key === '-') {
-          // zoom out
-          zoomMap(-1)
-        }
-      }
-    },
-    [!!data]
-  )
-
-  useEffect(() => {
-    window.addEventListener('keydown', handler)
-
-    return () => {
-      window.removeEventListener('keydown', handler)
-    }
-  }, [handler])
-
   useEffect(() => {
     if (!mapPropsInitialized.current && chunkBounds) {
       setMapProps(getMapProps(chunkBounds, projection))
@@ -102,8 +46,7 @@ const Map = () => {
   return (
     <MapContainer
       sx={{ width: '100%', mx: [4], mb: [3] }}
-      onDrag={panMap}
-      onScroll={zoomMap}
+      setMapProps={setMapProps}
     >
       {clim && (
         <Minimap {...mapProps} projection={PROJECTIONS[projection]}>
@@ -171,7 +114,6 @@ const Map = () => {
           {loading || url ? <Loading /> : 'Provide a Zarr link to explore data'}
         </Box>
       ) : null}
-      <Zoom zoomOut={() => zoomMap(-1)} zoomIn={() => zoomMap(1)} />
     </MapContainer>
   )
 }
