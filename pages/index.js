f28afda116ac1e7da14888d7329f8e84d06ebe40
katamartin <katasm@gmail.com>
2023-03-10 16:14:53 -0800

    Use sidebar component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Row, Column } from '@carbonplan/components'
-import { Box, Container, Divider, Flex } from 'theme-ui'
+import { Sidebar, SidebarDivider } from '@carbonplan/layouts'
+import { Box, Container, Flex } from 'theme-ui'
 import Dataset from '../components/dataset'
 import Display from '../components/display'
 
@@ -23,30 +24,14 @@ const Index = () => {
       >
         <Container>
           <Row>
-            <Column
-              start={1}
-              width={4}
-              sx={{
-                mx: [-4, -5, -5, -6],
-                px: [4, 5, 5, 6],
-                borderColor: 'muted',
-                borderWidth: 0,
-                borderRightWidth: 1,
-                borderStyle: 'solid',
-                height: '100vh',
-                pb: 4,
-                overflow: 'scroll',
-              }}
-            >
-              <Divider sx={{ mt: '56px', mx: [-4, -5, -5, -6], mb: 4 }} />
+            <Sidebar expanded side='left' width={4}>
               <Flex sx={{ flexDirection: 'column', gap: 3 }}>
                 <Dataset />
                 <Selectors />
               </Flex>
-              <Divider sx={{ mx: [-4, -5, -5, -6], my: 4 }} />
+              <SidebarDivider sx={{ my: 4 }} />
               <Display />
-              <Divider sx={{ mx: [-4, -5, -5, -6], my: 5 }} />
-            </Column>
+            </Sidebar>
             <Column start={[5]} width={[8]} sx={{ mt: '56px' }}>
               <Map />
             </Column>
