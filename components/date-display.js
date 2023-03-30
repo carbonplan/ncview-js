37f58b442633d48cf66209a5f6f80b9e9106a6d7
katamartin <katasm@gmail.com>
2023-03-30 12:30:35 -0700

    Refactor DateDisplay for potential reuse

--- components/date-display.js
+++ components/date-display.js
@@ -2,30 +2,29 @@ import { formatDate } from '@carbonplan/components'
 import { useCallback } from 'react'
 import useStore from './store'
 
-const DateDisplay = ({ array, selector, chunkShape }) => {
-  const { name, index, chunk } = selector
+export const useDateFormatter = (coordinate, options) => {
   const { units, calendar } = useStore(
-    (state) => state.metadata.metadata[`${name}/.zattrs`]
+    (state) => state.metadata.metadata[`${coordinate}/.zattrs`]
   )
+
+  if (!units) {
+    return
+  }
+  if (calendar !== 'proleptic_gregorian') {
+    console.warn(`Unhandled calendar: ${calendar}`)
+    return
+  }
+
+  const startDate = units.match(/(?<=days since ).+/)
+  if (!startDate) {
+    console.warn(
+      `No date found in: ${units}. Expected 'days since [DATE STRING]' format.`
+    )
+    return
+  }
 
   const formatter = useCallback(
     (v) => {
-      if (!units) {
-        return
-      }
-      if (calendar !== 'proleptic_gregorian') {
-        console.warn(`Unhandled calendar: ${calendar}`)
-        return
-      }
-
-      const startDate = units.match(/(?<=days since ).+/)
-      if (!startDate) {
-        console.warn(
-          `No date found in: ${units}. Expected 'days since [DATE STRING]' format.`
-        )
-        return
-      }
-
       let dateString = startDate[0]
       // append time to use local time zone if time is not already present
       if (!dateString.match(/\d\d:\d\d/)) {
@@ -35,11 +34,21 @@ const DateDisplay = ({ array, selector, chunkShape }) => {
       const date = new Date(dateString)
       date.setDate(date.getDate() + v)
 
-      return formatDate(date.toLocaleDateString())
+      return formatDate(date.toLocaleDateString(), options)
     },
-    [units, calendar]
+    [startDate, options]
   )
 
+  return formatter
+}
+
+const DateDisplay = ({ array, selector, chunkShape }) => {
+  const { index, chunk } = selector
+
+  const formatter = useDateFormatter(selector.name)
+  if (!formatter) {
+    return null
+  }
   return formatter(Number(array.data[index + chunk * chunkShape]))
 }
 
