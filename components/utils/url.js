a0275490dd5a67fab61b6ba3ddc1b039ad084041
katamartin <katasm@gmail.com>
2024-05-30 12:35:05 -0400

    Manually extract pathname from non-standard URLs
    Avoids browser-specific differences in URL construction

--- components/utils/url.js
+++ components/utils/url.js
@@ -1,15 +1,15 @@
 // Adapted from https://github.com/carbonplan/ncviewjs-backend/blob/27fa99c3f8aeaa7225589153ef9a85735697a9fd/ncviewjs_backend/helpers.py#L9
 
-const s3ToHttps = (pathname) => {
-  const parts = pathname.replace('//', '').split('/')
+const s3ToHttps = (url) => {
+  const parts = url.replace('s3://', '').split('/')
   const bucket = parts[0]
   const remainder = parts.slice(1).join('/')
 
   return `https://${bucket}.s3.amazonaws.com/${remainder}`
 }
 
-const gsToHttps = (pathname) => {
-  const trimmedPath = pathname.replace('//', '')
+const gsToHttps = (url) => {
+  const trimmedPath = url.replace('gs://', '')
 
   return `https://storage.googleapis.com/${trimmedPath}`
 }
@@ -24,9 +24,9 @@ export const sanitizeUrl = (url) => {
     case 'http:':
       return sanitized
     case 's3:':
-      return s3ToHttps(parsed.pathname)
+      return s3ToHttps(sanitized)
     case 'gs:':
-      return gsToHttps(parsed.pathname)
+      return gsToHttps(sanitized)
     // case 'az':
     // case 'abfs':
     // case 'abfss':
