e2cc151a055eeba19def470ae1e9daf9ef591cf1
Kata Martin <katasm@gmail.com>
2025-01-16 15:57:54 -0500

    Skip check for pyramids

--- components/utils/data.js
+++ components/utils/data.js
@@ -270,17 +270,19 @@ export const getVariableLevelInfo = async (
   const axes = ['X', 'Y'].reduce((accum, key, i) => {
     const index = dimensions.indexOf(cfAxes[name][key])
     const array = coordinates[i]
-    const stepValues = array.data.reduce((a, d, i) => {
-      if (i === 0) return a
-      const delta = d - array.data[i - 1]
-      a.add(delta)
-      return a
-    }, new Set())
-    const stepDeviation = deviation(stepValues)
-    if (stepDeviation && stepDeviation > 1e-10) {
-      throw new Error(
-        `Cannot handle coordinate: ${cfAxes[name][key]}. Spatial coordinates must be equally spaced.`
-      )
+    if (!pyramid) {
+      const stepValues = array.data.reduce((a, d, i) => {
+        if (i === 0) return a
+        const delta = d - array.data[i - 1]
+        a.add(delta)
+        return a
+      }, new Set())
+      const stepDeviation = deviation(stepValues)
+      if (stepDeviation && stepDeviation > 1e-10) {
+        throw new Error(
+          `Cannot handle coordinate: ${cfAxes[name][key]}. Spatial coordinates must be equally spaced.`
+        )
+      }
     }
 
     const step = Math.abs(Number(array.data[0]) - Number(array.data[1]))
